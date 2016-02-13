075e3c6032683a3284c5d223c767b538ed10e9a9
unknown <chenbojian@foxmail.com>
2016-02-13 15:53:10 +0800

    update

--- Tampermonkey-scripts/hacg.user.js
+++ Tampermonkey-scripts/hacg.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         hacg by cbj
 // @namespace    http://your.homepage/
-// @version      0.1.2
+// @version      0.1.3
 // @description  enter something useful
 // @author       You
 // @match        http*://hacg.lol/*
@@ -10,16 +10,16 @@
 // ==/UserScript==
 
 var content = $('.entry-content');
-content.each(function() {
-    var newContent = $(this).html().replace(/[A-Za-z0-9]{40}/g, '<div class = "magnet">magnet:?xt=urn:btih:$&</div>');
-    newContent = newContent.replace(/\/s\//, 'http://pan.baidu.com/s/');
-    $(this).html(newContent);
+content.each(function(i, el) {
+    var newContent = $(el).html().replace(/[A-Za-z0-9]{40}/g, '<div class = "magnet">magnet:?xt=urn:btih:$&</div>');
+    newContent = newContent.replace(/(\/s\/\w+) /, 'http://pan.baidu.com$0');
+    $(el).html(newContent);
 });
 var magnet = $('.magnet');
 magnet.css('color','blue');
 
 magnet.click(function(){
 	var allMagnets = [];
-    $(".magnet").each(e => allMagnets.push(e.html()));
+    $(".magnet").each((i, el) => allMagnets.push(el.html()));
     console.log(allMagnets);
 });

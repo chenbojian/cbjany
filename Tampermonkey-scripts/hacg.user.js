2020962e875ff121d5485ef18de0cc58c3a4419b
unknown <chenbojian@foxmail.com>
2016-02-13 15:19:04 +0800

    update hacg

--- Tampermonkey-scripts/hacg.user.js
+++ Tampermonkey-scripts/hacg.user.js
@@ -4,18 +4,21 @@
 // @version      0.1
 // @description  enter something useful
 // @author       You
-// @match        https://hacg.club/*
+// @match        https://hacg.lol/*
 // @grant        none
 // ==/UserScript==
 
 var content = $('.entry-content');
 content.each(function() {
     var newContent = $(this).html().replace(/[A-Za-z0-9]{40}/g, '<div class = "magnet">magnet:?xt=urn:btih:$&</div>');
+    newContent = newContent.replace(/\/s\//, 'http://pan.baidu.com/s/');
     $(this).html(newContent);
 });
 var magnet = $('.magnet');
 magnet.css('color','blue');
 
 magnet.click(function(){
-    console.log(this);
+	var allMagnets = [];
+    $(".magnet").each(e => allMagnets.push(e.html()));
+    console.log(allMagnets);
 });

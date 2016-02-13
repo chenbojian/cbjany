b5874c3223fec6451fc7010e9402a155cd7f24be
unknown <chenbojian@foxmail.com>
2016-02-13 17:49:48 +0800

    update hacg

--- Tampermonkey-scripts/hacg.user.js
+++ Tampermonkey-scripts/hacg.user.js
@@ -1,25 +1,45 @@
 // ==UserScript==
 // @name         hacg by cbj
 // @namespace    http://your.homepage/
-// @version      0.1.3
+// @version      0.1.5
 // @description  enter something useful
 // @author       You
 // @match        http*://hacg.lol/*
 // @match        http*://*.hacg.lol/*
-// @grant        none
 // ==/UserScript==
 
-var content = $('.entry-content');
-content.each(function(i, el) {
-    var newContent = $(el).html().replace(/[A-Za-z0-9]{40}/g, '<div class = "magnet">magnet:?xt=urn:btih:$&</div>');
-    newContent = newContent.replace(/(\/s\/\w+) /g, 'http://pan.baidu.com$1');
-    $(el).html(newContent);
-});
-var magnet = $('.magnet');
-magnet.css('color','blue');
+var tool = $('<div class="cbj-hacg-tool"></div>');
+$('body').append(tool);
 
-magnet.click(function(){
-	var allMagnets = [];
-    $(".magnet").each((i, el) => allMagnets.push(el.html()));
-    console.log(allMagnets);
+tool.css({
+	cursor: 'pointer',
+	position: 'fixed',
+	right: '20px',
+	bottom: '20px',
+	width: '20px',
+	height: '20px',
+	backgroundColor: 'white'
 });
+
+var content = $('article').html();
+
+tool.click(function() {
+	var magnets = content.match(/[A-Za-z0-9]{40}/g).map(m => `magnet:?xt=urn:btih:${m}`);
+	var baiduReg = /(\/s\/\w+) (\w+)/g;
+	var result, baiduLinks = [];
+	while(result = baiduReg.exec(content)) {
+		baiduLinks.push(`<a href="http://pan.baidu.com${result[1]}">${result[1]}</a> ${result[2]}`);
+	}
+
+    if($('.cbj-abstract').length === 0){
+        buildAbstract(magnets.concat(baiduLinks));
+    } else {
+        $('.cbj-abstract').remove();
+    }
+})
+
+function buildAbstract(magnets) {
+    var list = magnets.map(m => `<li>${m}</li>`).join('');
+    var div = $(`<div class="cbj-abstract"><ul>${list}</ul></div>`);
+    $('.entry-content').prepend(div);
+}

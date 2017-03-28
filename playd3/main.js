4284c2a7e2e81c1663569115cb91e14e6c185c71
Bojian Chen <bjchen@thoughtworks.com>
2017-03-28 18:14:27 +0800

    change directory

--- playd3/main.js
+++
@@ -1,8 +0,0 @@
-var data = [1,2,3,4,5,6];
-
-d3.select(".chart")
-  .selectAll("div")
-    .data(data)
-  .enter().append("div")
-    .style("width", function(d) { return d * 10 + "px"; })
-    .text(function(d) { return d; });
